2019b2e52a81c901622142a119ab728019a803d1
Todd Blank <toddcblank@gmail.com>
2019-11-24 18:16:00 -0600

    rolling back a bit to test some things

--- webserver.fsx
+++ webserver.fsx
@@ -8,9 +8,9 @@
 open Fake
 open Suave
 open Suave.Successful 
-// open Suave.Http.Successful
+open Suave.Http.Successful
 open Suave.Web
-// open Suave.Types
+open Suave.Types
 open System.Net
 open App
 
@@ -22,5 +22,12 @@ let serverConfig =
     let port = getBuildParamOrDefault "port" "8083" |> Sockets.Port.Parse
     { defaultConfig with bindings = [ HttpBinding.create HTTP IPAddress.Loopback port ] }
   
-startWebServer serverConfig App.Router
+// startWebServer serverConfig App.Router
+
+startWebServer serverConfig 
+    (OK  
+        ("Hello World! It's Suave.io on Azure Websites.<br /><ul>" + 
+          "<li>Sample git Repo: <a href='https://github.com/shanselman/suavebootstrapper'>https://github.com/shanselman/suavebootstrapper</a></li>" +
+          "<li>Intro blog post: <a href='http://www.hanselman.com/blog/RunningSuaveioAndFWithFAKEInAzureWebAppsWithGitAndTheDeployButton.aspx'>http://www.hanselman.com/blog/RunningSuaveioAndFWithFAKEInAzureWebAppsWithGitAndTheDeployButton.aspx</a></li>" +
+          "</ul>"))
 
